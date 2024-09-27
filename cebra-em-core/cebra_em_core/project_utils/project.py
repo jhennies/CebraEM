f901b4b79c359751f02da751051e9ab0dbf37ed7
jhennies <julianhennies@hotmail.de>
2024-09-27 10:01:43 +0200

    implementing removal of segmentations

--- cebra-em-core/cebra_em_core/project_utils/project.py
+++ cebra-em-core/cebra_em_core/project_utils/project.py
@@ -196,9 +196,9 @@ def remove_workflow_files(name, project_path=None, verbose=False, debug=False):
 
     if verbose:
         print('\nRemoving these files:\n')
-        print(filepaths)
-        for fp in filepaths:
-            print(fp)
+        print([os.path.split(fp)[1] for fp in filepaths])
+        # for fp in filepaths:
+        #     print(fp)
         print('')
 
 
