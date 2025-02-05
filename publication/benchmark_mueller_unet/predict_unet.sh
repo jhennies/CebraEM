9a894f67489c44ecf9ad2d54413cf807db2c2b68
jhennies <julianhennies@hotmail.de>
2025-02-05 14:57:10 +0100

    prediction of mueller et al unet

--- publication/benchmark_mueller_unet/predict_unet.sh
+++ publication/benchmark_mueller_unet/predict_unet.sh
@@ -7,8 +7,8 @@
 #SBATCH -n 4
 #SBATCH -t 1-0:00:00
 #SBATCH --mem=16G
-#SBATCH -e train-unet.err
-#SBATCH -o train-unet.out
+#SBATCH -e predict-unet.err
+#SBATCH -o predict-unet.out
 
 module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
 module load scikit-image/0.19.3-foss-2022a
