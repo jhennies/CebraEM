faab69be6338540ea373d9c228afe423d4c22ea0
jhennies <julianhennies@hotmail.de>
2025-02-05 16:11:26 +0100

    training script for all organelles of one dataset

--- publication/benchmark_mueller_unet/train_all.sh
+++ publication/benchmark_mueller_unet/train_all.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 repo_path=$1
-input_dirs=$2
+input_dirs=$2  # e.g. path/to/gt-hela-1
+run_name=$3  # e.g. unet-00
+output_dirpath=$4  # e.g. path/to/unets-hela-1
 
 echo repo_path = $repo_path
 
 for dirpath in $input_dirs; do
   echo processing $dirpath
+
+  echo sbatch ${repo_path}/train_unet.sh $dirpath $(basename "$dirpath")-${run_name} ${output_dirpath}
+
 done
 
