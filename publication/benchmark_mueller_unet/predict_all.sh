62464d32234a66b45cf906fc162f251a0516731a
jhennies <julianhennies@hotmail.de>
2025-04-03 09:43:35 +0200

    prediction of mueller unets

--- publication/benchmark_mueller_unet/predict_all.sh
+++ publication/benchmark_mueller_unet/predict_all.sh
@@ -9,19 +9,25 @@ sbatch /g/icem/hennies/src/github/jhennies/CebraEM-benchmark/CebraEM/publication
 
 
 repo_path=$1
-input_dirs=$2  # e.g. path/to/gt-hela-1
-run_name=$3  # e.g. unet-00
-output_dirpath=$4  # e.g. path/to/unets-hela-1
+input_files=$2  # e.g. path/to/gt-hela-1
+#run_name=$3  # e.g. unet-00
+#output_dirpath=$4  # e.g. path/to/unets-hela-1
 
 echo repo_path = $repo_path
 
-for dirpath in $input_dirs; do
+for filepath in $input_files; do
   echo ''
-  echo processing $dirpath
-  echo ''
-  echo sbatch ${repo_path}/train_unet.sh $dirpath $(basename "$dirpath")-${run_name} ${output_dirpath}
-  echo ''
-  sbatch ${repo_path}/train_unet.sh $dirpath $(basename "$dirpath")-${run_name} ${output_dirpath}
+  echo processing $filepath
 
 done
+#
+#for dirpath in $input_dirs; do
+#  echo ''
+#  echo processing $dirpath
+#  echo ''
+#  echo sbatch ${repo_path}/train_unet.sh $dirpath $(basename "$dirpath")-${run_name} ${output_dirpath}
+#  echo ''
+#  sbatch ${repo_path}/train_unet.sh $dirpath $(basename "$dirpath")-${run_name} ${output_dirpath}
+#
+#done
 
